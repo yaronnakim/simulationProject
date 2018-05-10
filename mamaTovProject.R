5e2a1a0f6d03954be1317a97833bd44567034ba3
ירון נקים <yaronnak@auth.ad.bgu.ac.il>
2018-05-10 14:03:27 +0300

    10.5.18 yaron commit

--- mamaTovProject.R
+++ mamaTovProject.R
@@ -13,12 +13,50 @@ ktzitzState <- function(){          #attribute for ktzitza state: 1 - proper, 2 
   return(state)
 }
 
+waitForLaneFinish <- function(){   
+  while (get_queue_count(mamara, "station1") > 0 &
+         get_queue_count(mamara, "station2") > 0 &
+         get_queue_count(mamara, "station3") > 0 &
+         get_queue_count(mamara, "station4") > 0 &
+         get_queue_count(mamara, "station5") > 0 
+  )
+  {
+   # log_("im waiting...")
+  }
+  timeout(function() 20)
+}
 
+waitWithSeize <- function(){  #optioenal
+  #we can generate break that seize all he stations and then send the workers to 
+  #break and after release the resources
+ 
+  deactivate("main")%>%
+  seize("station1", 1)%>%
+  seize("station2", 1)%>%
+  seize("station3", 1)%>%
+  seize("station4", 1)%>%
+  seize("station5", 1)%>%
+  timeout(function() 20)%>%
+  release("station1", 1)%>%
+  release("station2", 1)%>%
+  release("station3", 1)%>%
+  release("station4", 1)%>%
+  release("station5", 1)%>%
+  activate("main")
+}
+
+randomSample <- function() {
+  
+}
 
 
 ##----------------------------------------- 2.  all simulation parameters ------------------------------------------------
 
-simulationTime<-2*8*60 #daily model
+simulationTime<-2*8*60 #daily model - how much does the sim need to run
+# how we separate the times of the sim to know in witch hour of the day we are??
+# cus we need to know how much to pay for mullfunction 500 or 1000?
+
+QAinventory <- 0
 
 ##----------------------------------------- 3.  Init Simulation and add all resources  ------------------------------------------------
 
@@ -28,7 +66,11 @@ mamara <- simmer("mamara")%>%
   add_resource("station3", capacity=1, queue_size=Inf)%>%
   add_resource("station4", capacity=1, queue_size=Inf)%>%
   add_resource("station5", capacity=1, queue_size=Inf)%>%
-  add_resource("oven", capacity = 10, queue_size = Inf)
+  add_resource("oven", capacity = 10, queue_size = Inf)%>%
+  add_resource("workerBpacking", capacity = 1, queue_size = Inf)%>%  #the resource worker b take whille he pack
+  add_resource("workerBdeliver", capacity = 1, queue_size = Inf)%>%  #the resource worker b take when worker A need inventory refill
+  add_resource("workerA", capacity = 1, queue_size = Inf)
+
 
 ##----------------------------------------- 4.  All trajectories, start from main trajectory and add sub-trajectories ABOVE IT it . ------------------------------------------------
 
@@ -54,39 +96,47 @@ baking <-
   addService("oven", function() 10)
 
 
-packingTray <- 
-  trajectory("packingTray")%>% 
-  batch(n=20, timeout=0, permanent = FALSE)
-timeout = (function() rnorm(1, 2, 5))
-
-packingBox <-
-  trajectory("packingBox")%>%
-  batch(n=10, timeout=0, permanent = FALSE)
-
-
-needBreak <-                  #after 4 hours of work the workers go to rest
-  trajectory("needBreak")%>%
-  deactivate("main")%>%
-  while (get_queue_count(mamara, "station1") > 0 &
-         get_queue_count(mamara, "station2") > 0 &
-         get_queue_count(mamara, "station3") > 0 &
-         get_queue_count(mamara, "station4") > 0 &
-         get_queue_count(mamara, "station5") > 0 
-  )
-  {
-    b = 1
-  }
-timeout = 20
+packingTrayAndBox <- 
+  if(QAinventory < 100) {
+    set_capacity("workerBpacking", 0)%>%
+    set_capacity("workerBdeliver", 1)
+    
+  }else{
+    trajectory("packingTray")%>% 
+    batch(n=20, timeout=0, permanent = FALSE)%>%
+    timeout(function() rnorm(1, 2/60, 0.001/60))%>%
+    batch(n=10, timeout=0, permanent = FALSE)
+    }
 
 
-set_capacity("station1", 0)
+
+# needBreak <-                  #after 4 hours of work the workers go to rest for 20 min and after return to work all together
+#   trajectory("needBreak")%>%
+#   deactivate("main")%>%
+#   function()waitForLaneFinish()%>%
+#   activate("main")
+
+QAworkerNeedRefill <-         #worker b leave his station and bring a plat to qa inventory
+  trajectory("needRefill")%>%
+  set_capacity("workerBpacking",0)%>%
+  set_capacity("workerBdeliver",1)%>%
+  QAinventory = (QAworkerNeedRefill + 100) %>%
+  timeout(function() 4)
+  
+  
+
 
 breakTimeOverGoToWork <-      #rest time is over
   trajectory("finishRest")%>%
   set_capacity("station1", 1)
 
 
-qTest <- 
+QAtest <-                     #traj for testing the condition of the ktzitzot
+  trajectory("QAtest")%>%
+  separate()%>%
+  
+  randomSample100 <- function() randomSample
+  
   
   
   
@@ -95,7 +145,8 @@ qTest <-
   trajectory("main")%>%
   simmer::join(basicLine,
                baking,
-               packing
+               packing,
+               QAtest
   )
 
 
@@ -103,24 +154,30 @@ qTest <-
 
 
 ##----------------------------------------- 5.  All Generators, ALWAYS LAST. ------------------------------------------------
-
+#main generator for ktzitz
 add_generator(mamara, "main", distribution=function() 8/60,mon=2)
-add_generator(mamara, "needBreak", at(320))
+#generator for the first break
+add_generator(mamara, "firstNeedBreak", at(320))
+#generator for the seconed break
+add_generator(mamara, "seconedNeedBreak", at(720))
+#2 gennearators for mullfunctions with the acording distribution from fitDist_project file
+add_generator(mamara, "mullfunctionAD", distrubution = rnorm(1,normFit$estimate[1], normFit$estimate[2], mon = 2) )
+add_generator(mamara, "mullfunctionEJ", distrubution = rexp(1, expFit$estimate)) 
 
 ##----------------------------------------- 6.  reset, run, plots, outputs ------------------------------------------------
 set.seed(456)
-reset(Matnas)%>%
+reset()%>%
   simmer::run(simulationTime)
 
 
 
-MatnasData<-get_mon_arrivals(Matnas)%>%
+MatnasData<-get_mon_arrivals()%>%
   mutate(waiting_time = end_time - start_time - activity_time)
 
-ResourcesData <- get_mon_resources(Matnas)
-attributeData<-get_mon_attributes(Matnas)
+ResourcesData <- get_mon_resources()
+attributeData<-get_mon_attributes()
 
 #plots 
-plot(Gym)
+plot()
 
-arrivalsPerResource <- get_mon_arrivals(Matnas, per_resource=T)
+arrivalsPerResource <- get_mon_arrivals()
